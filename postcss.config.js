1caccde74a024e1e1843e3b6129c19b129dd1163
Ikanyeng <ikanyengadams2@gmail.com>
2024-10-07 10:35:17 +0200

    Updated next dependency to 14.2.14, modified postcss.config.js and tailwind.config.js files

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 
-module.exports = {
-  plugins: [
-    'postcss-import',
-    'tailwindcss',
-    'autoprefixer',
-  ],
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
